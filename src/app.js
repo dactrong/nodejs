01ee1ac53d0b35e4c3ce3acf431986b118c4b603
dactrong <trongndph13723@fpt.edu.vn>
2022-03-10 15:21:49 +0700

    update buoi2

--- src/app.js
+++ src/app.js
@@ -1,34 +1,17 @@
 // const http = require('http');
-const express = require("express");
+// const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import productRoute from './routes/product';
+import morgan from 'morgan';
 const app = express();
-app.get('/',(req, res) =>{
-    res.send("<h1>Home page</h1>")
-})
-app.get('/api/products', (req , res) =>{
-    const data = [
-        {id:1 , name : "Product A"},
-        {id:2 , name : "Product B"}
-    ];
-    res.json(data);
-})
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
 
-//     console.log(url);
-//     if (url === "/api/product") {
-//         const data = [
-//             { id: 1, name: "Product A" },
-//             { id: 2, name: "Product B" }
-//         ];
-//         res.end(JSON.stringify(data))
-//     } else if (url === "/api/ports") {
-//         console.log("API post");
-//     } else {
-//         res.setHeader("Content-Type", "text/html");
-//         res.write("<html><body><h1>Helo homepage</h1></body></html>");
-//         res.end();
-//     }
-// })
+app.use(cors())
+// midleware
+app.use(morgan('tiny'))
+
+app.use("/api",productRoute)
+//connect
 const PORT = 3001;
 server.listen(PORT, () => {
     console.log("Chay server thanh cong", PORT);
---
+++ src/routes/product.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { filter } from "minimatch";
+const router = Router();
+
+const checkAuth = (req , res, next) =>{
+    const isAdmin = true;
+    if(isAdmin){
+        next();
+    }else{
+        console.log('Lượn');
+    }
+}
+
+router.get('/product', checkAuth , (req , res) =>{
+    res.json(data);
+   
+});
+router.post('/product', checkAuth , (req , res) =>{
+    data.push(req.body);
+    console.log(data);
+    res.json(data);
+   
+});
+router.delete('/product/:id', checkAuth , (req , res) =>{
+    res.json(data/filter(item => item.id != req.params.id));
+   
+});
+router.put('/product/:id', checkAuth , (req , res) =>{
+    const result = data.map(item => item.id == req.params.id ? req.body : item)
+    res.json(result)
+   
+});
+export default router;
